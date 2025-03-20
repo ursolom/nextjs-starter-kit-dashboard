6e59e05362b5046020c46f1ca00a88656aa13c87
Soliman <soft9.me@gmail.com>
2025-03-20 16:13:55 +0200

    add starcusher

---
+++ src/lib/session.ts
@@ -0,0 +1,25 @@
+import { SignJWT, jwtVerify } from "jose";
+
+const key = ""
+
+
+export async function encrypt(payload) {
+    return new SignJWT
+}
+
+export async function refreshToken(token) {
+    const refreshToken = token
+    return refreshToken
+}
+
+export async function createSession() {
+
+}
+
+export async function verifySession() {
+
+}
+
+export async function deleteSession() {
+
+}
