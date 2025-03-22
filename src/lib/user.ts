0fcf61e71b644b4c3687febb3b168c8bf7979a60
Soliman <soft9.me@gmail.com>
2025-03-22 16:52:20 +0200

    fix logic

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -5,14 +5,14 @@ export const getUser = async () => {
     const session = await verifySession();
     if (!session.success) return null;
 
-    if (session.role === "ADMIN") {
-        return await db.user.findUnique({
-            where: { id: session.userId, role: "ADMIN" },
-        });
-    } else if (session.role === "USER") {
-        return await db.user.findUnique({
-            where: { id: session.userId, role: "USER" },
-        });
-    }
-    return null;
+    const user = await db.user.findUnique({
+        where: { id: session.userId },
+    });
+
+    if (!user) return null;
+
+    return {
+        ...user,
+        isAdmin: user.role === "ADMIN",
+    };
 };
