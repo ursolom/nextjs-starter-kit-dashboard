01839b02874d75dc0d026eb1b691eeeeb1544b5e
Soliman <soft9.me@gmail.com>
2025-03-30 01:32:51 +0200

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 import { LOGGED_OUT_ROUTES, PAGES, PUBLIC_ROUTES } from "./constants";
 import { cookies } from "next/headers";
-import { decrypt, refreshSession } from "./lib/session";
+import { cookieConfig, decrypt, refreshSession } from "./lib/session";
 import { redirectMiddleware } from "./helpers";
 import { Role } from "@prisma/client";
 
 export default async function middleware(req: NextRequest) {
     const { pathname } = req.nextUrl;
     const cookieStore = await cookies();
-    const cookie = cookieStore.get("session")?.value;
+    const cookie = cookieStore.get(cookieConfig.name)?.value;
     const reqHeader = new Headers(req.headers);
     reqHeader.set("x-url", req.url);
     const response = NextResponse.next({ request: { headers: reqHeader } });
@@ -24,8 +24,8 @@ export default async function middleware(req: NextRequest) {
     }
     // ---------------------------pages
     const isAdminRoute = pathname.startsWith(PAGES.ADMIN.DASHBOARD);
-    const protectedPages = ["account", "admin"];
-    const isProtectedPage = protectedPages.some(page => pathname.startsWith(`/${page}`));
+    const protectedPagesStart = ["account", "admin"];
+    const isProtectedPage = protectedPagesStart.some(page => pathname.startsWith(`/${page}`));
     // ---------------------------
 
     // Redirect authenticated users from logged-out routes
